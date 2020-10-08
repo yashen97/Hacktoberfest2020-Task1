c8652b5d3efc6de8dc2f214a808d49cf59f7a075
Yashen Raveesha Samarasingha <45714091+yashen97@users.noreply.github.com>
2020-10-08 10:34:30 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main()
 {
     char name,country,skill;
+    int age;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
     scanf("%d",&name);
